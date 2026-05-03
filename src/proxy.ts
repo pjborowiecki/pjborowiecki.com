b35508d6d275f255ba9b06d06cd7767d216d9213
P J Borowiecki <hello@pjborowiecki.com>
2026-05-04 01:55:56 +0200

    ✨ feat(i18n): improve next-intl configuration

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,26 @@
+import { type NextRequest, NextResponse } from "next/server"
+
 import createMiddleware from "next-intl/middleware"
 
 import { routing } from "~/src/integrations/next-intl/i18n.routing"
 
-export default createMiddleware(routing)
+const intlMiddleware = createMiddleware(routing)
+
+const ignoredPathsRegex = /^\/(?:dashboard)(?:\/|$)/
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+
+  if (ignoredPathsRegex.test(pathname)) {
+    const localeCookie = req.cookies.get("NEXT_LOCALE")?.value
+    const locale = routing.locales.find((l) => l === localeCookie) ?? routing.defaultLocale
+
+    req.nextUrl.pathname = `/${locale}${pathname}`
+    return NextResponse.rewrite(req.nextUrl)
+  }
+
+  return intlMiddleware(req)
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
